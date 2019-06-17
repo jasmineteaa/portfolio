fb13cac7f40b19fa7414689c0a32a9d9222843dc
jasmine <jasmine.tossan@gmail.com>
2019-06-17 16:22:10 -0400

    updated portfolio and about me section

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
+$(document).ready(function() {
+  // smoothscroll
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      500,
+      'linear'
+    )
+  })
+  // typed.js
+  // type animation in header
+  var typed = new Typed('#typed', {
+    stringsElement: '#typedStrings',
+    typeSpeed: 100,
+    backDelay: 900,
+    loop: true,
+    loopCount: Infinity
+  });
+  
+  
+  var typed = new Typed(".typed", options);
 
-$('a[href*="#"]').on('click', function(e) {
-  e.preventDefault()
-
-  $('html, body').animate(
-    {
-      scrollTop: $($(this).attr('href')).offset().top,
-    },
-    500,
-    'linear'
-  )
-})
-
-var typed = new Typed('#typed', {
-  stringsElement: '#typedStrings',
-  typeSpeed: 100,
-  backDelay: 900,
-  loop: true,
-  loopCount: Infinity
 });
 
-
-// var options = {
-//   strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-//   typeSpeed: 40
-// }
-
-var typed = new Typed(".typed", options);
