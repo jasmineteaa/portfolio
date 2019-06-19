c2978e5c4e10a8d6035421ea0a1188c5903c6211
jasmine <jasmine.tossan@gmail.com>
2019-06-19 04:58:21 -0400

    final responsive fixes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  $("#menuIcon").on('click', function () {
+    console.log('clicked!');
+    $(this).toggleClass("active")
+    $("nav").animate({
+      width: "toggle"
+    });
+    
+  });
+
   // smoothscroll
   $('a[href*="#"]').on('click', function(e) {
     e.preventDefault()
